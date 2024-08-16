d748cf546f524bbe34f2d3527ca30a4985584b78
pejmankheyri <pejmankheyri@gmail.com>
2024-08-16 11:16:02 +0200

    refactor: load-profile page

--- mep-app/pages/clusters/[id].vue
+++ mep-app/pages/clusters/[id].vue
@@ -35,7 +35,6 @@ const FTEProfileId = ref();
 const employeeChangeId = ref();
 const changeId = ref();
 const tabIndex = ref();
-const todaysFTE = ref([]);
 
 onMounted(async () => {
   await updateData();
@@ -43,16 +42,6 @@ onMounted(async () => {
 const list = ref();
 function updateCluster(id) {
   cluster.load_profiles[0].competence.push(id);
-}
-
-async function attachCompetence(competenceId, loadProfileId) {
-  try {
-    await loadProfileStore.attachCompetence(loadProfileId, competenceId);
-    await updateCompetences();
-  } catch (e) {
-    console.log(e);
-  } finally {
-  }
 }
 
 async function deleteCompetence() {
@@ -115,44 +104,38 @@ async function updateCompetences() {
   }
 }
 
-const editLoadProfileChange = (id, profileId) => {
-  changeId.value = id;
-  FTEProfileId.value = profileId;
+const editLoadProfileEvent = (id) => {
+  loadProfileId.value = id;
+  editLoadProfile.value = true;
+  isOpenAddLoadProfile.value = true;
+};
+
+const deleteLoadProfileEvent = (id) => {
+  loadProfileId.value = id;
+  isOpenDeleteLoadProfile.value = true;
+};
+
+const deleteLoadProfileCompetenceEvent = (event) => {
+  competenceId.value = event.competenceId;
+  loadProfileId.value = event.loadProfileId;
+  isOpenDeleteCompetence.value = true;
+};
+
+const addLoadProfileFTEEvent = (event) => {
   isOpenChangeFTE.value = true;
+  changeId.value = "";
+  FTEProfileId.value = event;
 };
 
-const todaysFTEShow = (id, value) => {
-  todaysFTE.value.push({ id, value });
+const editLoadProfileChangeEvent = (event) => {
+  isOpenChangeFTE.value = true;
+  changeId.value = event.id;
+  FTEProfileId.value = event.profileId;
 };
 
-// computed property for get todays FTE value from the array of todaysFTE values for the selected load profile
-const getTodaysFTE = (loadProfile) => {
-  return todaysFTE.value.find((item) => item.id === loadProfile.id)?.value;
-};
-
-const loadItems = (item) => {
-  return [
-    {
-      label: "organisation_load",
-      percentage: item.organisation_load,
-      color: "#32A546",
-    },
-    {
-      label: "base_load",
-      percentage: item.base_load,
-      color: "#05806d",
-    },
-    {
-      label: "local_load",
-      percentage: item.local_load,
-      color: "#FFCC00",
-    },
-    {
-      label: "comprehensive_load",
-      percentage: item.comprehensive_load,
-      color: "#E7324C",
-    },
-  ];
+const deleteLoadProfileFTEEvent = (event) => {
+  changeId.value = event;
+  isOpenDeleteChange.value = true;
 };
 </script>
 
@@ -290,280 +273,22 @@ const loadItems = (item) => {
                   }"
                 >
                   <template #item="{ item }">
-                    <div class="sub-container-gray flex flex-col">
-                      <div class="flex justify-between">
-                        <LoadProfileSearch
-                          @newCompetence="attachCompetence($event, item.id)"
-                        />
-                        <div class="w-[70px] flex justify-between align-middle">
-                          <UIcon
-                            name="i-line-md-edit-twotone-full"
-                            class="w-6 h-6 icon-edit self-center"
-                            dynamic
-                            @click="
-                              editLoadProfile = true;
-                              loadProfileId = item.id;
-                              isOpenAddLoadProfile = true;
-                            "
-                          />
-                          <UDivider orientation="vertical" />
-                          <UIcon
-                            name="i-line-md-document-remove-twotone"
-                            class="w-6 h-6 icon-delete self-center"
-                            dynamic
-                            @click="
-                              isOpenDeleteLoadProfile = true;
-                              loadProfileId = item.id;
-                            "
-                          />
-                        </div>
-                      </div>
-                      <div class="self-center flex justify-center w-full">
-                        <UCarousel
-                          v-slot="{ item }"
-                          :items="[
-                            { loadProfile: item },
-                            {
-                              fullTimeEmployees: {
-                                fte: item.full_time_employees,
-                                profileId: item.id,
-                                employeeChanges: item.employee_changes,
-                              },
-                            },
-                          ]"
-                          :ui="{
-                            wrapper: 'w-full',
-                            item: 'w-full',
-                            container: 'rounded-md flex flex-col w-full',
-                          }"
-                        >
-                          <template
-                            v-if="Object.keys(item)[0] === 'loadProfile'"
-                          >
-                            <div
-                              class="flex flex-col gap-4 justify-start align-middle pt-10 w-full"
-                            >
-                              <load-profile-competence
-                                @newCompetence="
-                                  attachCompetence(
-                                    $event,
-                                    item['loadProfile'].id
-                                  )
-                                "
-                              >
-                                <template #competence>
-                                  <template
-                                    v-for="competence in item['loadProfile']
-                                      .competences"
-                                    :key="competence.id"
-                                  >
-                                    <div
-                                      class="bg-white p-2 rounded flex justify-between border-2 border-gray-300"
-                                    >
-                                      <p
-                                        class="text-sm font-medium truncate pt-0.5"
-                                      >
-                                        {{ competence.name }}
-                                      </p>
-                                      <div class="pt-0.5 cursor-pointer">
-                                        <UIcon
-                                          name="i-heroicons-x-mark-20-solid "
-                                          @click="
-                                            isOpenDeleteCompetence = true;
-                                            loadProfileId =
-                                              item['loadProfile'].id;
-                                            competenceId = competence.id;
-                                          "
-                                        />
-                                      </div>
-                                    </div>
-                                  </template>
-                                </template>
-                              </load-profile-competence>
-
-                              <div v-if="isLoadingCompetences">
-                                <cluster-list-skeleton />
-                              </div>
-
-                              <PercentageBar
-                                v-else
-                                :items="loadItems(item['loadProfile'])"
-                              />
-
-                              <span class="text-sm pt-4">
-                                {{
-                                  $t("FTE_FOR_TODAY") +
-                                  ": " +
-                                  getTodaysFTE(item["loadProfile"])
-                                }}
-                              </span>
-
-                              <!-- <competence-chart
-                                v-else
-                                :base_load="item['loadProfile'].base_load"
-                                :comprehensive_load="
-                                  item['loadProfile'].comprehensive_load
-                                "
-                                :local_load="item['loadProfile'].local_load"
-                                :organisation_load="
-                                  item['loadProfile'].organisation_load
-                                "
-                              ></competence-chart> -->
-                            </div>
-                          </template>
-                          <div v-else class="border-t-2 mt-8 w-full">
-                            <div
-                              class="flex flex-col justify-start align-middle pt-10 w-full"
-                            >
-                              <UButton
-                                type="button"
-                                color="akq-green"
-                                icon="i-heroicons-plus-circle"
-                                class="justify-center text-base rounded mb-3 break-words"
-                                :ui="{
-                                  link: 'break-words',
-                                }"
-                                @click="
-                                  isOpenChangeFTE = true;
-                                  changeId = '';
-                                  FTEProfileId =
-                                    item['fullTimeEmployees'].profileId;
-                                  changeId = '';
-                                "
-                              >
-                                <span class="text-wrap truncate">{{
-                                  $t("NEW_CHANGE_FTES")
-                                }}</span>
-                              </UButton>
-
-                              <FullTimeEmployeesChart
-                                :clusterId="id.id"
-                                :profileId="item['fullTimeEmployees'].profileId"
-                                @todaysFTE="
-                                  todaysFTEShow(
-                                    item['fullTimeEmployees'].profileId,
-                                    $event
-                                  )
-                                "
-                              ></FullTimeEmployeesChart>
-                              <div>
-                                <div v-if="item['fullTimeEmployees'].fte">
-                                  <div class="grid grid-cols-4 gap-1 mt-8">
-                                    <div
-                                      v-for="change in loadProfiles.find(
-                                        (profile) =>
-                                          profile.id ===
-                                          item['fullTimeEmployees'].profileId
-                                      ).profile_changes"
-                                      :key="change.id"
-                                      class="bg-white p-4 rounded m-2 cursor-pointer hover:shadow-lg transition duration-300"
-                                      @click="
-                                        editLoadProfileChange(
-                                          change.id,
-                                          item['fullTimeEmployees'].profileId
-                                        )
-                                      "
-                                    >
-                                      <div class="flex flex-col">
-                                        <div>
-                                          <div
-                                            class="text-sm font-medium truncate"
-                                          >
-                                            {{
-                                              t("CHANGED_TO").concat(
-                                                " : ",
-                                                change.change || "---"
-                                              )
-                                            }}
-                                          </div>
-                                          <div
-                                            class="text-sm font-medium truncate"
-                                          >
-                                            {{
-                                              t("REASON").concat(
-                                                " : ",
-                                                change.reason
-                                              )
-                                            }}
-                                          </div>
-                                          <div
-                                            class="text-sm font-medium truncate"
-                                          >
-                                            {{
-                                              t("FROM").concat(
-                                                " ",
-                                                change.start_date
-                                              )
-                                            }}
-                                          </div>
-                                          <div
-                                            class="text-sm font-medium truncate"
-                                          >
-                                            {{
-                                              t("TO").concat(
-                                                " ",
-                                                change.end_date
-                                                  ? change.end_date
-                                                  : "---"
-                                              )
-                                            }}
-                                          </div>
-                                          <UDivider class="mt-3 mb-1" />
-
-                                          <div
-                                            class="w-full self-end flex justify-between align-middle"
-                                          >
-                                            <UButton
-                                              variant="link"
-                                              :label="t('EDIT')"
-                                              name="i-line-md-edit-twotone-full"
-                                              class="text-akq-green"
-                                              dynamic
-                                              @click="
-                                                editLoadProfileChange(
-                                                  change.id,
-                                                  item['fullTimeEmployees']
-                                                    .profileId
-                                                )
-                                              "
-                                            />
-                                            <UDivider orientation="vertical" />
-                                            <UButton
-                                              variant="link"
-                                              :label="t('DELETE')"
-                                              name="i-line-md-document-remove-twotone"
-                                              class="text-akq-red"
-                                              dynamic
-                                              @click.stop="
-                                                changeId = change.id;
-                                                isOpenDeleteChange = true;
-                                              "
-                                            />
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                                <div v-else>
-                                  <div
-                                    class="flex justify-center align-middle py-2"
-                                  >
-                                    <UIcon
-                                      name="i-heroicons-exclamation-circle "
-                                      class="w-6 h-6 pt-2 me-2 text-gray-600"
-                                    />
-                                    <span class="text-sm italic text-gray-600">
-                                      {{ t("FTE_INITIAL_VALUE_MSG") }}</span
-                                    >
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </UCarousel>
-                      </div>
-                    </div>
+                    <LoadProfileTabItem
+                      :item="item"
+                      :id="id"
+                      :loadProfiles="loadProfiles"
+                      :isLoadingCompetences="isLoadingCompetences"
+                      @editLoadProfile="editLoadProfileEvent($event)"
+                      @deleteLoadProfile="deleteLoadProfileEvent($event)"
+                      @deleteLoadProfileCompetence="
+                        deleteLoadProfileCompetenceEvent($event)
+                      "
+                      @addLoadProfileFTE="addLoadProfileFTEEvent($event)"
+                      @editLoadProfileChange="
+                        editLoadProfileChangeEvent($event)
+                      "
+                      @deleteLoadProfileFTE="deleteLoadProfileFTEEvent($event)"
+                    />
                   </template>
                 </UTabs>
               </section>
